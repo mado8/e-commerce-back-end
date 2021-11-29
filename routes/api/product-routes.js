f24cd26442557edc8c55c1a486658af738bd929a
mado <madelineadonley@gmail.com>
2021-11-29 01:06:17 -0700

    added category and product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {model: Category}, 
+        {model: Tag, through: ProductTag, as: 'product_tags'}
+      ]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {model: Category}, 
+        {model: Tag, through: ProductTag, as: 'product_tags'}
+      ]
+    });
+
+    if(!productData) {
+      return res.status(404).json({ message: 'Product ID not Found.' });
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
